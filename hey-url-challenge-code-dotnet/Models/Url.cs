2166e398951f3d17bf21400ca94f2922c6a5d254
Vitor Martini <80294295+vitor-martini@users.noreply.github.com>
2022-09-08 09:43:49 -0300

    Add files via upload

---
+++ hey-url-challenge-code-dotnet/Models/Url.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace hey_url_challenge_code_dotnet.Models
+{
+    public class Url
+    {
+        public Guid Id { get; set; }
+        public string ShortUrl { get; set; }
+        public int Count { get; set; }
+    }
+}
